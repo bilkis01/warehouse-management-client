5e976bfd891be3d67159c579d901120a88e4b554
suzan <asadsuzan7@gmail.com>
2022-05-07 16:45:08 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./Pages/Sheared/Footer/Footer";
 import Header from "./Pages/Sheared/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
